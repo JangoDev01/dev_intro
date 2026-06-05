89767a58818f4aa840533ba02b6b3ea83c724cfc
JangoDev01 <miguelgabriel072001@gmail.com>
2026-06-05 19:39:05 +0100

    criando a primeira view do projeto - responsavel por carregar a pagina inicial

--- django/to_do/views.py
+++ django/to_do/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+"""
+    View responsável por exibir a página inicial do aplicativo de tarefas. 
+    Atualmente, ela retorna uma mensagem simples de boas-vindas.
+"""
+def home(request):
+    return HttpResponse("Ola Mundo! Bem-vindo ao meu aplicativo de tarefas!")
