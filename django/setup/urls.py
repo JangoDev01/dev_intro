2519558bff91fee7151747a3ab75b187d44bbe27
JangoDev01 <miguelgabriel072001@gmail.com>
2026-06-07 15:30:03 +0100

    apagando a view home e criando a nova view todo_list. instalando o plugin do vscode django - pro vscode entender sintaxes do django. e configurando o plugin pra ser lido automaticamente apartir do json

--- django/setup/urls.py
+++ django/setup/urls.py
@@ -4,9 +4,6 @@ URL configuration for setup project.
 from django.contrib import admin
 from django.urls import path
 
-from to_do.views import home
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
 ]
--- django/to_do/views.py
+++ django/to_do/views.py
@@ -1,9 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-"""
-    View responsável por exibir a página inicial do aplicativo de tarefas. 
-    Atualmente, ela retorna uma mensagem simples de boas-vindas.
-"""
-def home(request):
-    return render(request, 'to_do/home.html')
+def todo_list(request):
+    return render(request, "to_do/todo_list.html")
