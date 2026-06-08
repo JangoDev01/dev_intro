b022a23209f17b5c3e7f36dd3c8b9630b589e56a
JangoDev01 <miguelgabriel072001@gmail.com>
2026-06-08 19:42:57 +0100

    apaganndo a view home  e subitituindo por todo_lit. e mais algumas configuracoes no json, que nao estao a funcionar assim tao bem

--- django/setup/urls.py
+++ django/setup/urls.py
@@ -4,6 +4,9 @@ URL configuration for setup project.
 from django.contrib import admin
 from django.urls import path
 
+from to_do.views import todo_list
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", todo_list),
 ]
