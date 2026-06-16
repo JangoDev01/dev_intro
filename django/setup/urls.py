95372b62aaf6664553f5f457f7797629e2c4c1df
JangoDev01 <miguelgabriel072001@gmail.com>
2026-06-16 17:50:28 +0100

    configurando as url de navegacao para cada view feita ate o momento

--- django/setup/urls.py
+++ django/setup/urls.py
@@ -8,7 +8,7 @@ from to_do.views import Todo_ListView, Todo_CreateView, Todo_BaseView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", Todo_BaseView.as_view()),
-    path("list", Todo_ListView.as_view()),
-    path("create", Todo_CreateView.as_view()),
+    path("", Todo_BaseView.as_view(), name="base"),
+    path("list", Todo_ListView.as_view(), name="list"),
+    path("create", Todo_CreateView.as_view(), name="create"),
 ]
