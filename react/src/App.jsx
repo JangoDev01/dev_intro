1700d6ef249dcd640e9c62bb0002d75f4694e925
JangoDev01 <miguelgabriel072001@gmail.com>
2026-06-25 23:20:37 +0100

    aditando o arquivo App.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,34 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  // STATES
   const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("Ola Mundo!")
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>{message}</h1>
       </div>
-      <h1>Vite + React</h1>
-      <p>Ola Mundo!</p>
+      <div className="card">
+        <button onClick={() => setMessage("Quebrei a Maldicao!")}>
+          Mudar Mensagem
+        </button>
+      </div>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
