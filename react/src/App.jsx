611d9330babf6fe937397fcbe957da9cfcc50724
JangoDev01 <miguelgabriel072001@gmail.com>
2026-06-26 16:17:44 +0100

    fiz muita coisa, mano... segue apenas os documentos com alteracoes desse commit que vais estar tudo de boas

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,36 +1,62 @@
-import { useState } from 'react'
-
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import { useState } from 'react'
 
 function App() {
   // STATES
-  const [count, setCount] = useState(0)
-  const [message, setMessage] = useState("Ola Mundo!")
+  const [tasks, sendTasks] = useState(
+    [
+      {
+        id: 1,
+        title: "Estudar Django.",
+        description: "Estudando pra usar no back-end do meu projeto de TCC.",
+        dateCreation: "26/06/2026",
+        deadline: "28/06/2026",
+        isCompleted: false,
+        isCompletedAt: "-/-/-"
+      },
+
+      {
+        id: 2,
+        title: "Estudar React.",
+        description: "Estudando pra usar no front-end web do meu projeto de TCC.",
+        dateCreation: "26/06/2026",
+        deadline: "28/06/2026",
+        isCompleted: false,
+        isCompletedAt: "-/-/-"
+      },
+
+      {
+        id: 3,
+        title: "Estudar React Native.",
+        description: "Estudando pra usar no front-end mobile do meu projeto de TCC.",
+        dateCreation: "26/06/2026",
+        deadline: "28/06/2026",
+        isCompleted: false,
+        isCompletedAt: "-/-/-"
+      }
+    ]
+  );
 
   return (
-    <>
+    // DIV PRINCIPAL DA APP
+    <div className='w-screen h-screen bg-slate-700 flex justify-center p-6'>
+      <div className='pr-8'>
+        
+      </div>
+
       <div>
-        <h1>{message}</h1>
-      </div>
-      <div className="card">
-        <button onClick={() => setMessage("Quebrei a Maldicao!")}>
-          Mudar Mensagem
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+          </h1>
+        <button>
+          Adicionar Tarefa
         </button>
-      </div>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-
-      <div className="card">
-        <h1 className="text-slate-500 text-3xl">Gerenciador de Tarefas</h1>
-        <Tasks />
+        
+        <Tasks tasks={tasks}/>
         <AddTask />
       </div>
-    </>
+    </div>
   )
 }
 
--- react/src/components/Tasks.jsx
+++ react/src/components/Tasks.jsx
@@ -1,7 +1,26 @@
-function Tasks(){
+import { ChevronRightIcon } from "lucide-react";
+
+function Tasks(props){
+    /**
+     * as props,nos ajudam a acessar componentes de outros lugares
+     * 
+     * no react, quando trabalhamos com listas, como no caso o "map", iterando cada elemento e 
+     * renderizando, devemos passar uma key...
+     * o react usa essa key internamente pra varias questoes, como perfonmance
+     */
+    console.log(props);
     return(
         <div>
-            <h1>Tarefas</h1>
+            <ul className="space-y-4 p-6 bg-slate-400 rounded-md shadow">
+                {props.tasks.map((task) => (
+                    <li key={task.id} className="flex gap-2">
+                        <p className="w-full bg-slate-500 text-white p-2 rounded-md">{task.title}</p>
+                        <button className="bg-slate-500 text-white p-2 rounded-md">
+                            <ChevronRightIcon />
+                        </button>
+                    </li> 
+                ))}
+            </ul>
         </div>
     );
 }
